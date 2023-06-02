2f0e1562b0a9ee270ddca0278e5d6556555461e3
kerkkanen <katja.kerkkanen@helsinki.fi>
2023-06-02 16:09:13 +0300

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Moi Is It Working?</h1>')
+  res.send('<h1>Hello, Is It Really Working?</h1>')
 })
 
 const PORT = 8080
