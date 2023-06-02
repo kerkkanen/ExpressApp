df03d78c8935a67b1f9f4c720c9410c517828472
kerkkanen <katja.kerkkanen@helsinki.fi>
2023-06-02 15:21:00 +0300

    Update text in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Changes</h1>')
+  res.send('<h1>Hello New Changes!</h1>')
 })
 
 const PORT = 8080
